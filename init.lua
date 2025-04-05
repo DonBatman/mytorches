14a7a741485c08820b2aa73c642a0b14e7070855
Don <serfdon@gmail.com>
2025-04-05 10:21:35 -0700

    Added settings

--- init.lua
+++ init.lua
@@ -1,5 +1,9 @@
-local torch_timer = 3600
+local torch_used = core.settings:get_bool("mytorches.torch_used", true)
+local torch_timer = tonumber(core.settings:get("mytorches.torch_time")) or 3600
 
+if torch_used then
+
+        
 minetest.override_item("default:torch", {
 	description = "Torch",
 	inventory_image = "",
@@ -59,7 +63,10 @@ on_place = function(itemstack, placer, pointed_thing)
 		y = p1.y - p0.y,
 		z = p1.z - p0.z
 		}
---	if p1 == "air" then
+		
+	local that = minetest.get_node(pointed_thing.above).name
+		
+	if that == "air" then
 		if p0.y>p1.y then
 			minetest.add_node(p1, {name="mytorches:torch_ceiling"})
 		elseif p0.y<p1.y then
@@ -67,7 +74,7 @@ on_place = function(itemstack, placer, pointed_thing)
 		else
 			minetest.add_node(p1, {name="mytorches:torch_wall", param2=minetest.dir_to_facedir(placer:get_look_dir())})
 		end
---	end
+	end
 	itemstack:take_item()
 		return itemstack
 end,
@@ -167,7 +174,7 @@ minetest.register_node("mytorches:torch_ceiling", {
 })
 ----------------------------------------------------------------------
 minetest.register_node("mytorches:torch_med", {
-	description = "Torch",
+--	description = "Torch",
 	inventory_image = "",
 	wield_image = "",
 	tiles = {
@@ -288,7 +295,7 @@ minetest.register_node("mytorches:torch_med_ceiling", {
 })
 ----------------------------------------------------------------------
 minetest.register_node("mytorches:torch_dim", {
-	description = "Torch",
+--	description = "Torch",
 	inventory_image = "",
 	wield_image = "",
 	tiles = {
@@ -405,7 +412,7 @@ minetest.register_node("mytorches:torch_dim_ceiling", {
 ----------------------------------------------------------------------
 ----------------------------------------------------------------------
 minetest.register_node("mytorches:torch_out", {
-	description = "Torch",
+--	description = "Torch",
 	inventory_image = "",
 	wield_image = "",
 	tiles = {
@@ -500,6 +507,159 @@ minetest.register_abm({
 			end
 		end
 })
+else
+------------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------------
+minetest.override_item("default:torch", {
+	description = "Torch",
+	inventory_image = "",
+	wield_image = "",
+	tiles = {
+		{name="mytorches_torch_top.png", animation={type="vertical_frames",
+		aspect_w=16, aspect_h=16, length=0.5}},
+		"mytorches_torch_bottom.png",
+		{name="mytorches_torch1.png", animation={type="vertical_frames",
+		aspect_w=16, aspect_h=16, length=0.5}},
+		{name="mytorches_torch1.png", animation={type="vertical_frames",
+		aspect_w=16, aspect_h=16, length=0.5}},
+		{name="mytorches_torch1.png", animation={type="vertical_frames",
+		aspect_w=16, aspect_h=16, length=0.5}},
+		{name="mytorches_torch1.png", animation={type="vertical_frames",
+		aspect_w=16, aspect_h=16, length=0.5}},
+	},
+	drawtype = "nodebox",
+	paramtype = "light",
+	paramtype2 = "facedir",
+	walkable = false,
+	damage_per_second = 1,
+	drop = "default:torch",
+	light_source = 14,
+	groups = {dig_immediate=3},
+	node_box = {
+		type = "fixed",
+		fixed = {
+			{-0.0625, -0.5, -0.03, 0.0625, 0.4375, 0.03},
+			{-0.03, -0.5, -0.0625, 0.03, 0.4375, 0.0625},
+			{-0.03, 0.3125, -0.03, 0.03, 0.5, 0.03},
+			{-0.095, 0.125, -0.0625, 0.095, 0.375, 0.0625},
+			{-0.0625, 0.125, -0.095, 0.0625, 0.3125, 0.095},
+		}
+	},
+	selection_box = {
+		type = "fixed",
+		fixed = {
+			{-0.0625, -0.5, -0.03, 0.0625, 0.4375, 0.03},
+			{-0.03, -0.5, -0.0625, 0.03, 0.4375, 0.0625},
+			{-0.03, 0.3125, -0.03, 0.03, 0.5, 0.03},
+			{-0.095, 0.125, -0.0625, 0.095, 0.375, 0.0625},
+			{-0.0625, 0.125, -0.095, 0.0625, 0.3125, 0.095},
+		}
+	},
+on_place = function(itemstack, placer, pointed_thing)
+	local timer = minetest.get_node_timer(pointed_thing.above)
+		timer:start(torch_timer)
+	if pointed_thing.type ~= "node" then
+		return itemstack
+	end
 
+	local p0 = pointed_thing.under
+	local p1 = pointed_thing.above
+	local dir = {
+		x = p1.x - p0.x,
+		y = p1.y - p0.y,
+		z = p1.z - p0.z
+		}
+		
+	local that = minetest.get_node(pointed_thing.above).name
+	if that == "air" then
+		if p0.y>p1.y then
+			minetest.add_node(p1, {name="mytorches:torch_ceiling"})
+		elseif p0.y<p1.y then
+			minetest.add_node(p1, {name="default:torch"})
+		else
+			minetest.add_node(p1, {name="mytorches:torch_wall", param2=minetest.dir_to_facedir(placer:get_look_dir())})
+		end
+	end
+	itemstack:take_item()
+		return itemstack
+end,
+})
 
+minetest.register_node("mytorches:torch_wall", {
+--	description = "Torch",
+	tiles = {
+		{name="mytorches_torch_top2.png", animation={type="vertical_frames",
+		aspect_w=16, aspect_h=16, length=0.5}},
+		"mytorches_torch_bottom2.png",
+		{name="mytorches_torch1.png", animation={type="vertical_frames",
+		aspect_w=16, aspect_h=16, length=0.5}},
+		{name="mytorches_torch1.png", animation={type="vertical_frames",
+		aspect_w=16, aspect_h=16, length=0.5}},
+		{name="mytorches_torch1.png", animation={type="vertical_frames",
+		aspect_w=16, aspect_h=16, length=0.5}},
+		{name="mytorches_torch1.png", animation={type="vertical_frames",
+		aspect_w=16, aspect_h=16, length=0.5}},
+	},
+	drawtype = "nodebox",
+	paramtype = "light",
+	paramtype2 = "facedir",
+	walkable = false,
+	drop = "default:torch",
+	damage_per_second = 1,
+	light_source = 14,
+	groups = {dig_immediate=3},
+	node_box = {
+		type = "fixed",
+		fixed = {
+			{-0.0625, 0.125, -0.03, 0.0625, 0.4375, 0.03},
+			{-0.03, 0.3125, -0.03, 0.03, 0.5, 0.03},
+			{-0.095, 0.125, -0.0625, 0.095, 0.375, 0.0625},
+			{-0.0625, 0.125, -0.095, 0.0625, 0.3125, 0.00},
+			{-0.03, 0.125, -0.0625, 0.03, 0.4375, 0.0625},
+			{-0.0625, 0.0625, -0.0625, 0.0625, 0.1875, 0.0625},
+			{-0.0625, 0, 0, 0.0625, 0.125, 0.125},
+			{-0.0625, -0.0625, 0.0625, 0.0625, 0.0625, 0.1875},
+			{-0.0625, -0.125, 0.125, 0.0625, 0, 0.25},
+			{-0.0625, -0.1875, 0.1875, 0.0625, -0.0625, 0.3125},
+			{-0.0625, -0.25, 0.25, 0.0625, -0.125, 0.375},
+			{-0.0625, -0.3125, 0.3125, 0.0625, -0.1875, 0.4375},
+			{-0.0625, -0.375, 0.375, 0.0625, -0.25, 0.5},
+		}
+	},
+})
+minetest.register_node("mytorches:torch_ceiling", {
+--	description = "Torch",
+	tiles = {
+		"mytorches_torch_bottom.png",
+		{name="mytorches_torch_top.png", animation={type="vertical_frames",
+		aspect_w=16, aspect_h=16, length=0.5}},
+		{name="mytorches_torch2.png", animation={type="vertical_frames",
+		aspect_w=16, aspect_h=16, length=0.5}},
+		{name="mytorches_torch2.png", animation={type="vertical_frames",
+		aspect_w=16, aspect_h=16, length=0.5}},
+		{name="mytorches_torch2.png", animation={type="vertical_frames",
+		aspect_w=16, aspect_h=16, length=0.5}},
+		{name="mytorches_torch2.png", animation={type="vertical_frames",
+		aspect_w=16, aspect_h=16, length=0.5}},
+	},
+	drawtype = "nodebox",
+	paramtype = "light",
+	paramtype2 = "facedir",
+	drop = "default:torch",
+	walkable = false,
+	damage_per_second = 1,
+	light_source = 14,
+	groups = {dig_immediate=3},
+	node_box = {
+		type = "fixed",
+		fixed = {
+			{-0.0625, -0.4375, -0.03, 0.0625, 0.5, 0.03},
+			{-0.03, -0.4375, -0.0625, 0.03, 0.5, 0.0625},
+			{-0.095, -0.125, -0.0625, 0.095, -0.375, 0.0625},
+			{-0.0625, -0.125, -0.095, 0.0625, -0.3125, 0.095},
+			
+		}
+	},
+})
 
+end
